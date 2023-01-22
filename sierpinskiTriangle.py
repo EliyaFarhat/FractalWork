d8749d09c804459a152b9597ced26f652fdc84c7
Eliya Farhat <91684882+EliyaFarhat@users.noreply.github.com>
2023-01-21 22:57:06 -0500

    Update sierpinskiTriangle.py

--- sierpinskiTriangle.py
+++ sierpinskiTriangle.py
@@ -1,36 +1,4 @@
-'''
-import random
-import matplotlib.pyplot as plt
 
-x = []
-y = []
-x.append(0)
-y.append(0)
-pc = 0
-lc = 0
-curr = 0
-
-for z in range(150000):
-
-    choice = random.randint(1,100)
-
-    if choice == 1:
-        x.append(0)
-        y.append(0.16 * (y[curr]))
-    if choice >= 2 and choice <= 86:
-        x.append(0.85 * (x[curr]) + 0.04 * (y[curr]))
-        y.append(-0.04 * (x[curr]) + 0.85 * (y[curr]) + 1.6)
-    if choice >= 94 and choice <= 100:
-        x.append(-0.15 * (x[curr]) + 0.28 * (y[curr]))
-        y.append(0.26 * (x[curr]) + 0.24 * (y[curr]) + 0.44)
-    if choice >= 87 and choice <= 93:
-        x.append(0.2 * (x[curr]) - 0.26 * (y[curr]))
-        y.append(0.23 * (x[curr]) + 0.22 * (y[curr]) + 1.6)
-    curr += 1
-
-plt.scatter(x, y, s = 0.2, edgecolor= 'green')
-plt.show()
-'''
 import pygame
 import math
 import sys
@@ -201,12 +169,6 @@ def drawTri(points, limit, colour, to):
     drawTri([A, AB, AC], limit - 1, [r, g, b], to)
     drawTri([AB, B, BC], limit - 1, [r, g, b], to)
     drawTri([AC, BC, C], limit - 1, [r, g, b], to)
-
-
-
-
-
-
 
 
 
